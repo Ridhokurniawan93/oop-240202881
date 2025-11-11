90e25666a567c0b121f0c6e7850e296789e12e30
rrkrniwnn <dalreng123@gmail.com>
2025-11-11 09:14:25 +0700

    Add files via upload

---
+++ praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/model/pembayaran/pembayaran.java
@@ -0,0 +1,21 @@
+package main.java.com.upb.agripos.model.pembayaran;
+
+public abstract class pembayaran {
+    protected String invoiceNo;
+    protected double total;
+
+    public pembayaran(String invoiceNo, double total) {
+        this.invoiceNo = invoiceNo;
+        this.total = total;
+    }
+
+    public abstract double biaya();               // fee/biaya tambahan
+    public abstract boolean prosesPembayaran();   // proses spesifik tiap metode
+
+    public double totalBayar() {
+        return total + biaya();
+    }
+
+    public String getInvoiceNo() { return invoiceNo; }
+    public double getTotal() { return total; }
+}
